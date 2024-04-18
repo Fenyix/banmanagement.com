5f02469f11e2e53c4386c7a11d5e0c566b4fd065
Fenyix <110176024+Fenyix@users.noreply.github.com>
2024-04-19 00:19:42 +0200

    g

--- constants/urls.js
+++
@@ -1,3 +0,0 @@
-export const GITHUB_ORG = 'https://github.com/BanManagement'
-export const DISCORD_INVITE = 'https://discord.gg/59bsgZB'
-export const DEMO = 'https://demo.banmanagement.com'
--- next-sitemap.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-  siteUrl: 'https://banmanagement.com',
-  generateRobotsTxt: true
-}
